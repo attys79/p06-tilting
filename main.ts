9b0708bd1186569cbbecc745580d2915a29ebbab
attys79 <75606949+attys79@users.noreply.github.com>
2020-12-09 13:53:26 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,21 @@
 basic.forever(function () {
-	
+    if (input.rotation(Rotation.Pitch) < -10) {
+        basic.showLeds(`
+            # # # # #
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (input.rotation(Rotation.Pitch) > 10) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            # # # # #
+            `)
+    } else {
+        basic.clearScreen()
+    }
 })
